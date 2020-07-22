a00664f2a217b2b43c59c1b125a4a59e0b034b2f
ZhengHaoYun <1013431730@qq.com>
2020-07-22 22:04:23 +0800

    update

---
+++ src/JZOffer/Solution3.java
@@ -0,0 +1,10 @@
+package JZOffer;
+
+/**
+ * @author ZhengHaoYun  2020/7/22 21:57
+ */
+public class Solution3 {
+    public boolean Find(int target, int[][] array) {
+        return false;
+    }
+}
--- src/leetcode/Solution3.java
+++ src/leetcode/Solution3.java
@@ -1,14 +1,34 @@
+package leetcode;
+
 import java.util.*;
 
-public class q3 {
+/**
+ * 3. 无重复字符的最长子串
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * <p>
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * <p>
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.lengthOfLongestSubstring2("abcdc"));
-    }
-}
-
-class Solution {
+public class Solution3 {
     /*
         方法一：双指针滑动窗口
         最坏情况下：每个元素都可能访问两次，复杂度为O(2N)，如：ancdc，每个元素都访问了两次。
