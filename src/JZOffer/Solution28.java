bd0c51c024a6e22a9df582fc0f8909ff33633988
ZhengHaoYun <1013431730@qq.com>
2020-08-20 17:05:11 +0800

    剑指 Offer 28. 对称的二叉树

---
+++ src/JZOffer/Solution28.java
@@ -0,0 +1,63 @@
+package JZOffer;
+
+/*
+剑指 Offer 28. 对称的二叉树
+请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+
+
+示例 1：
+
+输入：root = [1,2,2,3,4,4,3]
+输出：true
+示例 2：
+
+输入：root = [1,2,2,null,3,null,3]
+输出：false
+
+ */
+public class Solution28 {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        return recur(root.left, root.right);
+
+    }
+
+    public boolean recur(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+
+        return recur(left.left, right.right) && recur(left.right, right.left);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
