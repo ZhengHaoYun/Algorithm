823d04d2f69d3516b8cb1992d0660ca501d46aa1
ZhengHaoYun <1013431730@qq.com>
2020-08-24 19:43:45 +0800

    剑指 Offer 17. 打印从1到最大的n位数

---
+++ src/JZOffer/Solution17.java
@@ -0,0 +1,35 @@
+package JZOffer;
+
+/*
+剑指 Offer 17. 打印从1到最大的n位数
+输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+
+示例 1:
+
+输入: n = 1
+输出: [1,2,3,4,5,6,7,8,9]
+
+
+说明：
+
+用返回一个整数列表来代替打印
+n 为正整数
+ */
+public class Solution17 {
+
+
+    public static void main(String[] args) {
+        Solution17 solution17 = new Solution17();
+        solution17.printNumbers(1);
+    }
+
+    public int[] printNumbers(int n) {
+        int[] nums = new int[(int) Math.pow(10, n) - 1];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i + 1;
+        }
+        return nums;
+    }
+
+
+}
